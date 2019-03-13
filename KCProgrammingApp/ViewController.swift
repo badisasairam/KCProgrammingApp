4719463943c71f166096bacec749f206a36b276e
Badisa,Sairam <s534148@ol10018m.nwmissouri.edu>
2019-03-12 19:31:48 -0500

    Initial Commit

---
+++ KCProgrammingApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  KCProgrammingApp
+//
+//  Created by Badisa,Sairam on 3/12/19.
+//  Copyright © 2019 Badisa,Sairam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
